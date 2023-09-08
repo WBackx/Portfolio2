e29d8bea74a0d8c390461f1544e0552e3a627844
WBackx <115712968+WBackx@users.noreply.github.com>
2023-09-08 13:38:51 +0200

    chat gpt fix1

--- Portfolio2/form.php
+++ Portfolio2/form.php
@@ -8,46 +8,50 @@
 <body>
         <?php
 
+
 $servername = "localhost";
 $username = "Portfolio_Wouter";
 $password = "p%9Gfa811";
+$dbname = "Portfolio_Wb";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=Portfolio_Wb", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 
-        // Taking all 5 values from the form data(input)
-        $first_name = $_REQUEST['first-name'];
-        $last_name = $_REQUEST['last-name'];
-        $email = $_REQUEST['email'];
-        $subject = $_REQUEST['subject'];
-        $message = $_REQUEST['message'];
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Taking all 5 values from the form data(input)
+    $first_name = $_POST['first-name'];
+    $last_name = $_POST['last-name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-        // We are going to insert the data into our sampleDB table
-        $sql = "INSERT INTO Email_info VALUES ('$first_name',
-            '$last_name','$email','$subject','$message')";
-        echo ($sql);
-        echo (  $first_name);
-        echo ( $last_name);
-        echo ($email);
+    try {
+        // Prepare the SQL statement to insert data
+        $stmt = $conn->prepare("INSERT INTO Email_info (first_name, last_name, email, subject, message) 
+            VALUES (:first_name, :last_name, :email, :subject, :message)");
 
-        // Check if the query is successful
-        if(mysqli_query($conn, $sql)){
-            echo "<h3>data stored in a database successfully."
-                . " Please browse your localhost php my admin"
-                . " to view the updated data</h3>";
+        // Bind parameters to prepared statement
+        $stmt->bindParam(':first_name', $first_name);
+        $stmt->bindParam(':last_name', $last_name);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':subject', $subject);
+        $stmt->bindParam(':message', $message);
 
-            echo ("\n$first_name\n $last_name\n "
-                . "$email\n $subject\n $message");
-        } else{
-            echo "ERROR: Hush! Sorry $sql. "
-                . mysqli_error($conn);
-        }
+        // Execute the statement
+        $stmt->execute();
+
+        echo "<h3>Data stored in the database successfully.</h3>";
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
 
         // Close connection
         mysqli_close($conn);

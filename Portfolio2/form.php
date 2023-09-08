20728318ba67f9b4a5fe22a3964979888522c095
WBackx <115712968+WBackx@users.noreply.github.com>
2023-09-08 13:35:07 +0200

    sdsdsd

--- Portfolio2/form.php
+++ Portfolio2/form.php
@@ -8,11 +8,19 @@
 <body>
         <?php
 
+$servername = "localhost";
+$username = "Portfolio_Wouter";
+$password = "p%9Gfa811";
 
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=Portfolio_Wb", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 
-    $con = mysqli_connect("localhost", "root", "p%9Gfa811", "Portfolio_Wouter");
-
-    echo ($con);
         // Taking all 5 values from the form data(input)
         $first_name = $_REQUEST['first-name'];
         $last_name = $_REQUEST['last-name'];
@@ -23,7 +31,7 @@
         // We are going to insert the data into our sampleDB table
         $sql = "INSERT INTO Email_info VALUES ('$first_name',
             '$last_name','$email','$subject','$message')";
-        echo ($sql);
+        echo ($sql)
         echo (  $first_name);
         echo ( $last_name);
         echo ($email);
